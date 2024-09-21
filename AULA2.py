c2e6306ca365ef3419ae591c542b2762151b1af9
lil1994 <porfiriolillen@gmail.com>
2024-09-20 21:25:01 -0300

    Códigos Básicos | Aula 1 e 2

---
+++ AULA2.py
@@ -0,0 +1,58 @@
+"""print('digite a nota do primeiro bimestre')
+B1 = float(input())
+print('digite a nota do segundo bimestre')
+B2 = float(input())
+print('digite a nota do terceiro bimestre')
+B3 = float(input())
+print('digite a nota do quarto bimestre')
+B4 = float(input())
+media = (B1+B2+B3+B4)/4
+print('a media do aluno é', media)
+
+if media >= 7:
+    print('APROVADO')
+elif media >=5:
+    print('RECUPERAÇÃO')
+else:
+    print('REPROVADO')"""
+
+# Código para liberar acesso a festa
+"""print('qual é a sua idade?')
+idade = int(input())
+if idade < 18:
+    print('acesso negado! você é menor de idade')
+elif idade == 18:
+    print(' vc está quase lá! mais um ano e vc terá acesso!')
+else: 
+    print('acesso liberado! boa festa')"""
+
+# Código para verificar se pode participar do MT
+""""print('Qual é o seu gênero? (F ou M)')
+genero.upper() = input()
+print('Qual é o municipio você mora?')
+municipio = input()
+if genero == 'F' and municipio.lower()0 == 'rio de janeiro':
+    print('você pode participar!')
+else:
+    print('você não pode participar!')"""
+
+# Código para liberar acesso ao filme para maiores de 18 anos
+"""print('*' *30,'BEM VINDO AO CINEMA!','*' *30)
+print(' ')
+print('Qual é a sua idade?')
+idade = int(input())
+if idade >= 18:
+   print('Acesso liberado!\nBom filme!')
+elif idade >= 16:
+    print('O FILME SELECIONADO É PARA MAIOR DE 18 ANOS!\nPARA ACESSAR PRECISA ESTAR ACOMPANHADO DE UM RESPONSÁVEL.\nVOCÊ ESTÁ COM SEU RESPONSÁVEL?')
+    responsavel = input().upper()
+    if responsavel == 'SIM':
+        print('Acesso liberado!\nBom filme!')
+    else:
+            print('VOCÊ SÓ PODE VER O FILME ACOMPANHADO DE UM RESPONSÁVEL!')
+else:
+    print('ACESSO NEGADO!\nVOCÊ É MENOR!')"""
+
+
+
+    
